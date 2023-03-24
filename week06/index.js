f30e98bced7b55ca68f0aac7d487e18a3b9eb880
Jim Schmidt <palaestra.schmidt@gmail.com>
2023-03-24 08:50:24 -0400

    Checkpoint

---
+++ week06/index.js
@@ -0,0 +1,18 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+    url: 'redis://localhost:6379'
+  });
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+await client.connect();
+
+//await client.set('cat', 'meow');
+await client.incr('cat')
+const value = await client.get('cat');
+
+console.log(`got ${value} from redis`)
+
+await client.disconnect();
+
---
+++ week08/index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import redis from 'redis'
+const app = express()
+const port = 80         // port 80 is used for the http protocol
+
+const db = redis.createClient({
+    url: 'redis://localhost:6379'
+})
+await db.connect()
+
+app.use(express.static('palaestracoder/spring2023/week08/public'))
+
+app.get('/inc', async(req, res) => {
+    const count = await db.incr("count")
+    res.send(`count: ${count}`)
+})
+
+app.get('/dec', async(req, res) => {
+    const count = await db.incrBy("count", -1)
+    res.send(`count: ${count}`)
+})
+
+app.get('/echo/:value', (req, res) => {
+    res.send(req.params)
+})
+
+app.get('/save/:number', async (req, res) => {
+    await db.set("thenumber", req.params.number)
+    await db.
+    res.send("OK")
+})
+
+app.get('/showme', async (req, res) => {
+    const number = await db.get("thenumber")
+    res.send(`<!DOCTYPE html><html><body>${number}</body></html>`)
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
